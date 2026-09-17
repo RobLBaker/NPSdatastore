639e6cefc7e332b825bea47fb62d1ee633463084
Rob Baker <robert_baker@nps.gov>
2026-09-17 10:59:37 -0600

    degug get_unit_geography and add documentation

--- R/units_service.R
+++ R/units_service.R
@@ -26,6 +26,18 @@ get_all_nps_units <- function(dev = FALSE, verbose = FALSE) {
   return(all_units)
 }
 
+#' Get unit geography
+#'
+#' @param units String, List. One or more valid units (e.g. "ROMO" or c("ROMO", "YElL"))
+#' @param detail String. Defaults to "convexHull". Acceptable alternatives are "envelope" or "feature".
+#' @param dataformat String. Defaults to "wkt". An acceptable alternative is "glm"
+#' @param dev Logical. Defaults to FALSE. Should the dev API be used?
+#' @param verbose Logical. Defaults to FALSE. Should lots of information about the API call be printed to the console? Great for debugging.
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 get_unit_geography <- function(units,
                                detail = c("convexHull", "envelope", "feature"),
                                dataformat = c("wkt", "glm"),
@@ -35,8 +47,8 @@ get_unit_geography <- function(units,
   dataformat <- match.arg(dataformat)
   units <- toupper(units)
 
-  if (length(seq_along(units)) > 1) {
-    units <- paste(units, collapse = "; ")
+  if (length(units) > 1) {
+    units <- curl::curl_escape(paste(units, collapse = "; "))
   }
 
   unit_geo_url <- .get_base_units_url(is_dev = dev)
@@ -44,6 +56,7 @@ get_unit_geography <- function(units,
     httr2::req_url_path_append(units, "geography") |>
     httr2::req_url_query(detail = detail, dataformat = dataformat) |>
     httr2::req_headers(Accept = "application/json") |>
+    httr2::req_options(verbose = verbose) |>
     httr2::req_perform()
 
   unit_geo <- httr2::resp_body_json(unit_geo)
@@ -52,9 +65,7 @@ get_unit_geography <- function(units,
                                                      use.names = TRUE,
                                                      fill = TRUE))
   unit_geo <- tibble::as_tibble(unit_geo)
-
   return(unit_geo)
-
 }
 
 
